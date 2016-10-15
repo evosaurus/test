191c4763306c0d5cdd192895aae02315835ec2b9
evosaurus <ev3@xs4all.nl>
2016-10-15 22:50:51 +0200

    test

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,2 @@
-nrs=[2,1,3,2,1,2]
-# gaat goed met "If the length of the list is even the middle element is defined to
-# be the rightmost of the two elements in the middle of the list
-print(len(nrs)//2)
-print(nrs[len(nrs)//2])
-
-nrs.sort(reverse=True)
-###  nrs.reverse()
-print(nrs)
-
-nrs.sort()
-# nrs.reverse()
-print(nrs)
-nrs.append(nrs.pop(0))
-print(nrs)
+print('Weer gelukt!!')
 
